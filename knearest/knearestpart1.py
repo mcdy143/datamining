af6ca05fa0f5e527be02cb29a44d1e77cc90cbe2
Daniel Alabi <alabid@carleton.edu>
2013-04-05 16:23:07 -0500

    something

--- knearest/knearestpart1.py
+++ knearest/knearestpart1.py
@@ -3,6 +3,19 @@
 # standardizes the specified columns in writingportfolio.csv 
 
 import csv
+import numpy as np
+from scipy import stats
+
+def standardize(data):
+    newdata = []
+    data = np.array(data, dtype=np.float64)
+    stds = np.std(data, axis=0)
+    avgs = np.mean(data, axis=0)
+    
+    for line in data:
+        newdata.append((line-avgs)/stds)
+
+    return newdata
 
 if __name__ == "__main__":
     f = open("writingportfolio.csv")
@@ -11,7 +24,10 @@ if __name__ == "__main__":
     for row in reader:
         data.append(row)
     toprocess = range(1, len(row)-1)
-    for line in data:
-        print [i for i in range(len(line)) if i in toprocess]
-
-    # standardize(data)
+    newdata = []
+    for line in data[1:]:
+        newdata.append([line[i] for i in range(len(line)) if i in toprocess])
+    data = newdata
+    print standardize(data)
+    print stats.zscore(np.array(data))
+    
